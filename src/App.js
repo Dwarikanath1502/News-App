01aadc0a0374a02378293bb5537ba4b589596534
Dwarikanath1502 <ddwarikaanth15@gmail.com>
2022-02-26 05:31:42 -0500

    updated URLs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 export default class App extends Component {
-  name = "Dwarikanath"
+
   render() {
     return (
-      <div>Hello {this.name}</div>
+      <div>
+        <Navbar />
+        <News />
+        {/* <NewsItem /> */}
+      </div>
     )
   }
 }
