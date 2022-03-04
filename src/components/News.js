65c6561feaf018e8e7b9f17ab0cf7a276bdc1c51
Dwarikanath1502 <ddwarikaanth15@gmail.com>
2022-03-04 12:52:18 +0530

    added top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -28,10 +28,13 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aba0349b6d5d4fb594fedf08fb7bfae3&page={this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true })
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json()
+        this.props.setProgress(70);
         console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
@@ -39,6 +42,8 @@ export class News extends Component {
             loading: false,
 
         })
+        this.props.setProgress(100);
+
     }
 
     async componentDidMount() {
